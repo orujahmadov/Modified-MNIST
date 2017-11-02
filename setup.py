10cc54a93dbb3340bc1eea8cfb94a0412c0c7009
orujahmadov <oruc888@yahoo.com>
2017-11-01 20:36:13 -0400

    initiali commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['keras>=2.0.8','sklearn>=0.0', 'h5py>=2.7.0', 'google-cloud-storage>=1.5.0']
+REQUIRED_PACKAGES = ['sklearn>=0.0', 'h5py>=2.7.0', 'google-cloud-storage>=1.5.0']
 
 setup(
     name='trainer',
